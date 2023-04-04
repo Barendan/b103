0f79b337b981f9f749c8611df22f0be73d866643
Barendan <barendan@gmail.com>
2023-04-04 15:30:05 -0400

    add content for new posts

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,17 +1,56 @@
+import Image from 'next/image'
 import ReactMarkdown from 'react-markdown'
 
 import PostHeader from './post-header'
 import classes from './post-content.module.css'
 
-
 function PostContent(props) {
   const { post } = props;
   const imagePath = `/images/posts/${post.slug}/${post.image}`;
 
+  console.log('boom pop', post.slug, post.image)
+  
+  const customRenderers = {
+    // image(image) {
+    //   return (
+
+    //     <Image 
+    //     src={`/images/posts/${post.slug}/${image.src}`} 
+    //     alt={image.alt} 
+    //     width={600} 
+    //     height={300}
+    //     />
+    //   )
+    // },
+    paragraph(paragraph) {
+      const { node } = paragraph;
+      
+      if (node.children[0].type === 'image') {
+        console.log('explode', node.children[0])
+        const image = node.children[0];
+
+        return (
+          <div className={classes.image}>
+            
+            <Image 
+              src={`/images/posts/${post.slug}/${image.properties.src}`}
+              alt={image.alt} 
+              width={600} 
+              height={300}
+            />
+          </div>
+        )
+      }
+
+      return <p>{paragraph.children}</p>
+    }
+  }
+
   return (
     <article className={classes.content}>
+      {/* {console.log('custom', customRenderers)} */}
       <PostHeader title={post.title} image={imagePath} />
-      <ReactMarkdown>{post.content}</ReactMarkdown>
+      <ReactMarkdown renderers={customRenderers}>{post.content}</ReactMarkdown>
     </article>
   )
 }
