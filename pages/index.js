cc4acd4d409a672094351e8e1dacae2a448e5312
Barendan <barendan@gmail.com>
2023-04-08 11:23:43 -0400

    add metadata to pages

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import Head from 'next/head'
+
 import Hero from '../components/home-page/hero'
 import FeaturedPosts from '../components/home-page/featured-posts'
 import { getFeaturedPosts } from '@/lib/posts-util'
@@ -6,6 +8,13 @@ import { getFeaturedPosts } from '@/lib/posts-util'
 export default function HomePage(props) {
   return (
     <>
+    <Head>
+      <title>Danse Blog</title>
+      <meta 
+        name="description"
+        content="I love to post and roast"
+      />
+    </Head>
       <Hero />
       <FeaturedPosts posts={props.posts} />
     </>
